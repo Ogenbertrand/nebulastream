957798c30f71f5c210a0d2d309a8acb31342f0b7
Ogenbertrand <ogenbertrand@gmail.com>
2026-03-19 17:17:38 +0100

    export vidsrc resolver provider

--- apps/backend-api/services/providers/__init__.py
+++ apps/backend-api/services/providers/__init__.py
@@ -5,9 +5,11 @@ from services.providers.vidcloud_provider import get_streams as vidcloud_streams
 from services.providers.superembed_provider import get_streams as superembed_streams
 from services.providers.twoembed_provider import get_streams as twoembed_streams
 from services.providers.vidlink_provider import get_streams as vidlink_streams
+from services.providers.vidsrc_resolver_provider import get_streams as vidsrc_resolver_streams
 
 __all__ = [
     "vidsrc_streams",
+    "vidsrc_resolver_streams",
     "vidcloud_streams",
     "superembed_streams",
     "twoembed_streams",
